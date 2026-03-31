094979f174d3edc887f577f7d6ae95c54547cbfd
Henrik <htmdude@yahoo.com>
2026-03-30 23:24:13 -0600

    Add MateriasGrid component for subject distribution

---
+++ src/components/MateriasGrid.jsx
@@ -0,0 +1,8 @@
+export default function MateriasGrid() {
+  return (
+    <section className="materias-grid">
+      <h2>Materias</h2>
+      <p>Subject distribution cards will be rendered here.</p>
+    </section>
+  );
+}
