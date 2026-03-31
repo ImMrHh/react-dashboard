73d1bb8fb0d4210742a4dfbf1ca34bbd1139c3a3
Henrik <htmdude@yahoo.com>
2026-03-30 23:23:03 -0600

    Create ActivityAnalysis component for activity charts

---
+++ src/components/ActivityAnalysis.jsx
@@ -0,0 +1,8 @@
+export default function ActivityAnalysis() {
+  return (
+    <section className="activity-analysis">
+      <h2>Actividades</h2>
+      <p>Charts for activity types and top materias will be added here.</p>
+    </section>
+  );
+}
