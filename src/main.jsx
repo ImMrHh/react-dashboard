6889e53e0fd51b6033d01bc84af9d6f023f62a5b
Henrik <htmdude@yahoo.com>
2026-03-30 23:14:15 -0600

    Initialize main.jsx with React and DataProvider

---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App';
+import { DataProvider } from './context/DataContext';
+import './index.css';
+
+createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <DataProvider>
+      <App />
+    </DataProvider>
+  </React.StrictMode>
+);
