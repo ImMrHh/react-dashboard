e6e3025809ccec9950ea50aec61d9d4ee7eb1bd6
Henrik <htmdude@yahoo.com>
2026-03-30 23:23:38 -0600

    Add ProfessorsTable component

---
+++ src/components/ProfessorsTable.jsx
@@ -0,0 +1,8 @@
+export default function ProfessorsTable() {
+  return (
+    <section className="professors-table">
+      <h2>Profesores</h2>
+      <p>Top professors list will be rendered here.</p>
+    </section>
+  );
+}
